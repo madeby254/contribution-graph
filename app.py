54289ed313f3982e5eebc25f702e12a68356c8b2
madeby254 <mojitsu185@gmail.com>
2022-08-28 21:24:21 -0700

    5

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-import os
-from random import randint
+import os , random
 
-for i in range(1,365):
-
-  for j in range(0,randint(1,10)):
-    d=str(i) + 'days ago'
-    with open('file.txt', 'a') as file:
-        file.write(d)
-    os.system('git add .')
-    os.system('git commit --date="' + d + '"-m "commit"')
-
-  os.system('git push -u origin main')
+for i in range(200):
+    d = str(i) + 'days ago'
+    rand = random.randrange(1, 12)
+    with open('test.txt','a') as file:
+        file.write(d+'\n')
+    os.system('git add test.txt')
+    os.system('git commit --date=" 2020-'+str(rand)+'-'+d+'" -m 1')
+os.system('git push -u origin main')
